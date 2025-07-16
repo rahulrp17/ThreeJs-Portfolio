06cd133514df3a9d8dc78fd4268fa126cfc3fd93
rahulrp17 <rahulrp4021@gmail.com>
2025-07-16 14:55:40 +0530

    Update HomeInfo.jsx

--- src/pages/Components/HomeInfo.jsx
+++ src/pages/Components/HomeInfo.jsx
@@ -1,76 +1,48 @@
-import React from "react";
-import arrow from "../../assets/icons/arrow.svg";
-import { Link } from "react-router-dom";
-import './homeInfo.css'
+import React from 'react'
+import {project} from '../constants/index.js'
+import { Link } from 'react-router-dom'
+import { arrow } from '../assets/icons'
 
-
-const HomeInfo = ({ currentStage }) => {
-  if (currentStage === 1) {
-    return (
-      <h1 className="sm:text-2xl sm:leading-snug  md:h-30  text-center neo-brutalism-blue  py-4 px-8 mt-10 text-white">
-        HI, I'm
-        <span className="font-semibold mx-2 text-white">Rahul</span>
-        👋
-        <br />A Java Full Stack Developer from India
+const projects = () => {
+  return (
+    <div className=' max-w-5xl mx-auto sm:p-16 pb-12 !pt-[126px] px-8 min-h-[calc(100vh-80px)]'>
+      <h1 className='sm:text-5xl text-3xl font-semibold sm:leading-snug '>
+        My{" "}
+        <span className='blue-gradient_text  bg-gradient-to-r from-[#00c6ff] to-[#0072ff] bg-clip-text text-transparent font-semibold drop-shadow-2xl'>
+          Projects
+        </span>
       </h1>
-    );
-  }
-  if (currentStage === 2) {
-    return (
-      <div className="info-box mx-5 relative flex text-white flex-col  gap-3 max-w-2xl  neo-brutalism-blue pt-4 pb-10 px-8 mt-2.5 top-5">
-        <p className=" font-medium sm:text-xl text-center md:w-70 md:h-20">
-          Worked with many Companies <br /> and picked up many skills along the
-          way
-        </p>
+      
+      <p className='mt-2 text-justify text-gray-600 leading-8'>
+        A collection of full stack web development projects that demonstrate my ability to design, build, and deploy robust web applications from end to end. These projects highlight hands-on experience with front-end frameworks <b>(React, HTML/CSS),</b> back-end technologies <b>(Node.js, Express),</b> and databases <b>(MongoDB, Firebase, SQL).</b> Each project reflects my focus on responsive design, <b >RESTful API</b>  integration, user authentication, and performance optimization—bringing complete, scalable solutions to life.
+      </p>
 
-        <Link
-          to="/about"
-          className="neo-btn neo-brutalism-white py-2 px-6 rounded-lg text-blue-500 text-center font-semibold sm:w-1/2 s w-[90%] -bottom-5 absolute mx-auto right-0 left-0 flex justify-center items-center gap-3 ;"
-        >
-          Learn More
-          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
-        </Link>
+      <div className='flex my-20 flex-wrap  gap-10'>
+        {
+          project.map((project,index)=>{
+            return(
+              <div key={index} className='lg:w-[400px] w-full'>
+                  <div className='block-container w-12 h-12'>
+                    <div className="btn-back rounded-xl "></div>
+                    <div className="btn-front rounded-xl flex items-center justify-center">
+                      <img src={project.iconUrl}  className="w-10 h-100 object-contain" />
+                    </div>
+                  </div>
+                  <div className='mt-5 flex flex-col'>
+                     <h1 className='text-black font-poppins text-2xl font-bold'>{project.name}</h1>
+                     <p className='text-gray-500 mt-2'>{project.description}</p>
+                     <div className='mt-5 flex items-center gap-3 font-poppins'>
+                      <Link to={project.link} target='_blank' rel='noopener noreferrer' className='font-semibold text-blue-600 '>Live Link</Link>
+                      <img src={arrow} className='4-4 h-4 object-contain'/>
+                     </div>
+                  </div>
+              </div>
+            )
+          })
+        }
       </div>
-    );
-  }
+    </div>
+  )
+}
 
-  if (currentStage === 3) {
-    return (
-      <div className="info-box  neo-brutalism-blue   mx-5 relative flex text-white flex-col gap-3 max-w-120 mt-2.5 neo-brutalism-blue pt-4 pb-10 px-8 top-5">
-        <p className="font-medium sm:text-xl text-center md:w-80 md:h-20">
-          Led multiple projects to success over the year, <br /> Curious about
-          the impact?
-        </p>
-
-        <Link
-          to="/projects"
-          className="neo-btn neo-brutalism-white py-2 px-6 md:w-60  rounded-lg text-blue-500 text-center font-semibold sm:w-1/2 w-[90%] -bottom-5 absolute mx-auto right-0 left-0 flex justify-center items-center gap-3"
-        >
-          Visit my Project
-          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
-        </Link>
-      </div>
-    );
-  }
-
-  if (currentStage === 4) {
-    return (
-      <div className="info-box  neo-brutalism-blue mx-5 relative flex mt-2.5   text-white flex-col gap-3 max-w-110 neo-brutalism-blue pt-4 pb-10 px-8  top-5">
-        <p className="font-medium sm:text-xl text-center md:w-80 md:h-20">
-          Get in touch with me for any project ideas or just to say hi!
-        </p>
-
-        <Link
-          to="/contact"
-          className="neo-btn neo-brutalism-white  py-2 px-6   rounded-lg text-blue-500 text-center font-semibold sm:w-1/2 w-[90%] -bottom-5 absolute mx-auto right-0 left-0 flex justify-center items-center gap-3"
-        >
-          Let's Talk
-          <img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
-        </Link>
-      </div>
-    );
-  }
-  return null;
-};
-
-export default HomeInfo;
+export default projects
